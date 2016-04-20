34f1ba864571e4edd27fb1f42890584dc62d18d5
Hyeonkee-Jo <201202143@cs-cnu.org>
2016-04-20 12:24:34 +0900

    새로운알람이요

---
+++ app/src/main/java/com/example/jo930_000/albu/AlarmReceiver.java
@@ -0,0 +1,24 @@
+package com.example.jo930_000.albu;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by jo930_000 on 2016-04-20.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String state = intent.getExtras().getString("extra");
+        Log.e("MyActivity", "In the receiver with " + state);
+
+        Intent serviceIntent = new Intent(context,RingTone.class);
+        serviceIntent.putExtra("extra", state);
+
+        context.startService(serviceIntent);
+    }
+
+}
